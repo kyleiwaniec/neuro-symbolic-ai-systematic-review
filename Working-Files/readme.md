c198840dc3fecbe153537721566f90f357a52c5e
kyle Hamilton <kylehamilton@macbook-pro-4.home>
2022-02-16 08:42:50 +0000

    cleanup the repo for public consumption

---
+++ Working-Files/readme.md
@@ -0,0 +1,75 @@
+Scopus search
+=============
+
+Retrieved on July 20, 2021 4:47 PM GMT+1
+
+### Results: 2180 (2000 most relevant)
+
+ 
+
+KEYWORDS
+--------
+
+TITLE-ABS-KEY ( ( ( *"sub-symbolic"*  OR  *"machine learning"*  OR  *"deep
+learning"* )  AND  ( *"symbolic"*  OR  *"reasoning"*  OR  *"logic"* ) )  OR  ( *"neuro-symbolic"*  OR  *"neural-symbolic"*  OR  *"neuro
+symbolic"*  OR  *"neural
+symbolic"*  OR  *"neurosymbolic"* ) )  AND  PUBYEAR  \>  *2010*  AND  ( LIMIT-TO ( LANGUAGE ,  *"English"* ) )  AND  ( LIMIT-TO ( SUBJAREA ,  *"COMP"* ) )  AND  ( LIMIT-TO ( DOCTYPE ,  *"ar"* ) ) 
+
+[query link](https://www.scopus.com/results/results.uri?sort=plf-f&src=s&sid=eaa136b48638c30fd904ccfcee8ee983&sot=a&sdt=a&cluster=scosubjabbr%2c%22COMP%22%2ct%2bscosubtype%2c%22ar%22%2ct%2bscolang%2c%22English%22%2ct&sl=250&s=TITLE-ABS-KEY+%28+%28+%28+%22sub-symbolic%22+OR+%22machine+learning%22+OR+%22deep+learning%22+%29+AND+%28+%22symbolic%22+OR+%22reasoning%22+OR+%22logic%22+%29+%29+OR+%28+%22neuro-symbolic%22+OR+%22neural-symbolic%22+OR+%22neuro+symbolic%22+OR+%22neural+symbolic%22+OR+%22neurosymbolic%22+%29+%29+AND+PUBYEAR+%3e+2010&origin=searchadvanced&editSaveS=&txGid=70ecf65212fa837932f8968307da34fd)
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#### scopus.csv columns:
+
+"Key","Item Type","Publication Year","Author","Title","Publication
+Title","ISBN","ISSN","DOI","Url","Abstract Note","Date","Date Added","Date
+Modified","Access Date","Pages","Num Pages","Issue","Volume","Number Of
+Volumes","Journal Abbreviation","Short Title","Series","Series Number","Series
+Text","Series
+Title","Publisher","Place","Language","Rights","Type","Archive","Archive
+Location","Library Catalog","Call Number","Extra","Notes","File
+Attachments","Link Attachments","Manual Tags","Automatic Tags","Editor","Series
+Editor","Translator","Contributor","Attorney Agent","Book Author","Cast
+Member","Commenter","Composer","Cosponsor","Counsel","Interviewer","Producer","Recipient","Reviewed
+Author","Scriptwriter","Words By","Guest","Number","Edition","Running
+Time","Scale","Medium","Artwork Size","Filing Date","Application
+Number","Assignee","Issuing Authority","Country","Meeting Name","Conference
+Name","Court","References","Reporter","Legal Status","Priority
+Numbers","Programming Language","Version","System","Code","Code
+Number","Section","Session","Committee","History","Legislative Body"
+
+
+### Filtered by top publishers
+#### See notebook for details filterd-search-results.ipynb
+1,510 articles in `scopus-filtered-by-top-publication.csv`
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{'ACM',
+ 'American Association for the Advancement of Science',
+ 'American Chemical Society',
+ 'American Institute of Physics',
+ 'American Society for Microbiology',
+ 'Association for Computing Machinery',
+ 'Cairo University',
+ 'Chongqing University of Posts and Telecommunications',
+ 'Elsevier',
+ 'Emerald',
+ 'IEEE',
+ 'IOS Press',
+ 'Institute for Operations Research and the Management Sciences',
+ 'King Saud University',
+ 'MIT Press',
+ 'Mary Ann Liebert',
+ 'Morgan & Claypool Publishers',
+ 'Now Publishers Inc',
+ 'Now Publishers Inc.',
+ 'Optical Society of America',
+ 'Oxford University Press',
+ 'Public Library of Science',
+ 'SAGE',
+ 'Society for Industrial and Applied Mathematics',
+ 'Springer Nature',
+ 'Taylor & Francis',
+ 'University of California Press',
+ 'University of Minnesota',
+ 'Wiley-Blackwell'}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ Working-Files/utils.py
@@ -0,0 +1,134 @@
+# Top2Vec: BERT + HDBSCAN (https://towardsdatascience.com/topic-modeling-with-bert-779f7db187e6)
+# use SPECTER model https://arxiv.org/abs/2004.07180 (this model was trained on scientific papers)
+
+from sentence_transformers import SentenceTransformer
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import umap
+import hdbscan
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+import time
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def make_clusters(data, neighbors=15, components=5, cluster_size=5, save_embeddings=False):
+    """
+    input: data should be an array of strings or matrixes of floats
+           ex: data[0]==str, or data[0][0]==np.float
+    returns: HDBSCAN object
+    """
+    try:
+        if type(data[0])==str:
+            model = SentenceTransformer('allenai-specter')
+            embeddings = model.encode(data, show_progress_bar=True)
+            if save_embeddings:
+                print('saving embeddings')
+                ts=str(time.time())
+                np.savetxt('embeddings'+ts+'.txt', embeddings, delimiter=',')
+        elif type(data[0][0])==np.float64:
+            embeddings=data
+    except Exception as e: 
+        print(e)
+        return
+    
+    umap_embeddings = umap.UMAP(n_neighbors=neighbors, 
+                                n_components=components, 
+                                metric='cosine').fit_transform(embeddings)
+
+    cluster = hdbscan.HDBSCAN(min_cluster_size=cluster_size,
+                              metric='euclidean',                      
+                              cluster_selection_method='eom').fit(umap_embeddings)
+    return cluster
+
+def get_topics(cluster, abstracts, n_words=20):
+    """
+    input: 
+        cluster- HDBSCAN object created by make_clusters(), 
+        abstracts - array of str
+        n_words - number of top words to return for each topic
+    returns:
+        top_n_words - dictionary containing top words for each topic as key. topic -1 are outliers (do not belong to any topic)
+        topic_sizes - pandas dataframe containing the number of aricles per topic
+    """
+    
+    docs_df = pd.DataFrame(abstracts, columns=["Doc"])
+    docs_df['Topic'] = cluster.labels_
+    docs_df['Doc_ID'] = range(len(docs_df))
+    docs_per_topic = docs_df.groupby(['Topic'], as_index = False).agg({'Doc': ' '.join})
+
+
+    def c_tf_idf(documents, m, ngram_range=(1, 1)):
+        count = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(documents)
+        t = count.transform(documents).toarray()
+        w = t.sum(axis=1)
+        tf = np.divide(t.T, w)
+        sum_t = t.sum(axis=0)
+        idf = np.log(np.divide(m, sum_t)).reshape(-1, 1)
+        tf_idf = np.multiply(tf, idf)
+
+        return tf_idf, count
+
+    tf_idf, count = c_tf_idf(docs_per_topic.Doc.values, m=len(abstracts))
+
+    def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
+        words = count.get_feature_names()
+        labels = list(docs_per_topic.Topic)
+        tf_idf_transposed = tf_idf.T
+        indices = tf_idf_transposed.argsort()[:, -n:]
+        top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+        return top_n_words
+
+    def extract_topic_sizes(df):
+        topic_sizes = (df.groupby(['Topic'])
+                         .Doc
+                         .count()
+                         .reset_index()
+                         .rename({"Topic": "Topic", "Doc": "Size"}, axis='columns')
+                         .sort_values("Size", ascending=False))
+        return topic_sizes
+    
+    top_n_words = extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=n_words)
+    topic_sizes = extract_topic_sizes(docs_df);
+    
+    return top_n_words, topic_sizes
+
+def drawCloud(text,max_words=350,width=1000,height=600,figsize=(15,15),raw=True,title="",save=False,path='cloud.svg'):
+    wc = WordCloud(background_color="white", 
+                   max_words=max_words, 
+                   width=width, 
+                   height=height, 
+                   random_state=1)
+    if raw:
+        wc.generate(text)
+    else:    
+        wc.generate_from_frequencies(text)
+    
+    plt.figure(figsize=figsize)
+#     plt.title(title)
+    plt.axis("off")
+    cloud=plt.imshow(wc)
+    if(save):
+        plt.savefig(path, format="svg",  bbox_inches='tight')
+    return cloud
+
+
+def getTFIDF(text,top_n=15,maxdf=0.9):
+    vectorizer = TfidfVectorizer(stop_words='english',min_df=0.2,max_df=maxdf,ngram_range=(1,3),strip_accents='unicode')
+    X = vectorizer.fit_transform(text)
+#     print(X.shape)
+    
+    feature_names=vectorizer.get_feature_names()
+    words_scores=[]
+    for col in X.nonzero()[1]:
+        words_scores.append((feature_names[col],X[0, col]))
+    #     print(feature_names[col], ' - ', X[0, col])
+
+    words_scores=list(set(words_scores))
+    word_scores_sorted=defaultdict(str)
+    for item in sorted(words_scores, key = lambda x: -x[1])[:top_n]:
+        word_scores_sorted[item[0]]=item[1]
+#         print(item)
+    return word_scores_sorted
